a6df6feed07c08c6b4c7b3e6a1561fc1b03c966d
wuqiang <happyboy_tu@163.com>
2019-08-30 17:26:46 +0800

    新增全局样式文件，rem布局文件

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,8 +1,19 @@
+<!--
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-30 12:08:54
+ * @LastEditTime: 2019-08-30 17:12:34
+ * @LastEditors: Please set LastEditors
+ -->
 <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <div></div>
+      <a href="">1235455</a>
+      <div></div>
+      <input type="text">
     </div>
     <router-view/>
   </div>
@@ -14,7 +25,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #333;
+  font-size: px2rem(28);
 }
 #nav {
   padding: 30px;
--- code/vue.config.js
+++ code/vue.config.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-29 23:37:16
- * @LastEditTime: 2019-08-30 00:39:29
+ * @LastEditTime: 2019-08-30 17:01:01
  * @LastEditors: Please set LastEditors
  */
 // vue.config.js
@@ -17,12 +17,12 @@ module.exports = {
     loaderOptions: {
       sass: {
         // 设置css中引用文件的路径，引入通用使用的scss文件（如包含的@mixin）
-        // data: `
-        //   @import "assets/scss/variables.scss";
-        //   @import "assets/scss/function.scss";
-        //   @import "assets/scss/mixins.scss";
-        //   @import "assets/scsscommon.scss";
-        // `
+        data: `
+          @import "@/assets/scss/reset.scss";
+          @import "@/assets/scss/common.scss";
+          @import "@/assets/scss/function.scss";
+          @import "@/assets/scss/mixins.scss";
+        `
       }
     }
   }
